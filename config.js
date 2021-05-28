7b417ec3ff544114726ce27d5b4cbed39af64fd4
gratefulasi <84240101+gratefulasi@users.noreply.github.com>
2021-05-28 23:35:13 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // export const endpoint = `http://localhost:3000/api/graphql`;
 export const endpoint = `https://lifeseed-moon.herokuapp.com/api/graphql`;
 // export const endpoint = `https://lifeseed-moon.herokuapp.com/api/graphql`;
-export const prodEndpoint = `todo after deploy`;
+export const prodEndpoint = `https://lifeseed-moon.herokuapp.com/api/graphql`;
 export const perPage = 8;
